c62451f4b20c969c624bcd55c42f0e259139085b
Esteban Arrangoiz <esteban.arrangoiz@gmail.com>
2021-09-16 00:05:59 -0400

    completed exercise, geo location working

---
+++ ConsoleApp1/MapImage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Device.Location;
+
+namespace WhereAmI
+{
+    class MapImage
+    {
+        public static void Show(GeoCoordinate location)
+        {
+            string filename = $"{location.Latitude:##.000},{location.Longitude:##.000},{location.HorizontalAccuracy:####}m.jpg";
+
+            DownloadMapImage(BuildURI(location), filename);
+
+            OpenWithDefaultApp(filename);
+        }
+
+        private static void DownloadMapImage(Uri target, string filename)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(target, filename);
+            }
+        }
+
+        /// <summary>
+        /// Map Image REST API by HERE Location Services
+        /// </summary>
+        /// <remarks>
+        /// https://developer.here.com/
+        /// </remarks>
+        private static Uri BuildURI(GeoCoordinate location)
+        {
+            #region HERE App ID & App Code
+            string HereApi_AppID = "WAsABO6TbE9Tn8lQfptH";
+            string HereApi_AppCode = "8wadiqVKAZ5ltNK5q2i30iybLdsiifEKSs5xhwhwPow";
+            #endregion
+
+            //https://image.maps.ls.hereapi.com/mia/1.6/mapview?apiKey={YOUR_API_KEY}
+            var HereApi_DNS = "image.maps.ls.hereapi.com";
+            var HereApi_URL = $"https://{HereApi_DNS}/mia/1.6/mapview";
+            var HereApi_Secrets = $"&apiKey={HereApi_AppCode}";
+
+            var latlon = $"&lat={location.Latitude}&lon={location.Longitude}";
+
+            return new Uri(HereApi_URL + $"?u={location.HorizontalAccuracy}" + HereApi_Secrets + latlon);
+        }
+
+        private static void OpenWithDefaultApp(string filename)
+        {
+            var si = new ProcessStartInfo()
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/C start {filename}",
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+            Process.Start(si);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhereAmI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting GeoCoordinate Watcher...");
+
+            // 2. Use the GeoCoordinate Watcher
+            var watcher = new GeoCoordinateWatcher();
+
+            watcher.StatusChanged += (s, e) =>
+            {
+                Console.WriteLine($"GeoCoordinateWatcher:StatusChanged:{e.Status}");
+            };
+
+            watcher.PositionChanged += (s, e) =>
+            {
+                watcher.Stop();
+
+                Console.WriteLine($"GeoCoordinateWatcher:PositionChanged:{e.Position.Location}");
+
+                // 3. Use the Map Image REST API
+                MapImage.Show(e.Position.Location);
+            };
+
+            watcher.MovementThreshold = 100;
+
+            watcher.Start();
+
+            
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
